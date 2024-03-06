39d41c78872e63808ddae90a80c8e33b549f0d72
Mahendra Kumawat <117855423+mk016@users.noreply.github.com>
2024-03-06 11:06:28 +0000

    basic complete

--- PasswordGenertor/src/App.jsx
+++ PasswordGenertor/src/App.jsx
@@ -1,14 +1,57 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 
 
 import './App.css'
 
 function App() {
+ const [length, setLength ] = useState(8)
+ const [numberAllowed , setNumberAllowed] = useState(false);
+ const [charAllowed , setCharAllowed] = useState("")
 
+ // useRef hook
+
+ const passwordRef = useRef(null)
+
+ const passwordGenerator = useCallback(() =>{
+  let pass =""
+  let str =  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+  if (numberAllowed) str += "0123456789"
+  if (charAllowed) str += "!@#$%^&*-_+=[]{}~`"
+
+  for (let i =1; i<=length; i++){
+    let char = Math.floor(Math.random() * str.length +1)
+    pass += str.charAt(char)
+  }
+
+   setPassword(pass)
+
+ }, [length, numberAllowed , charAllowed, setPassword]
+ )
+
+ const copyPasswordToClipboard = useCallback (() => {
+  passwordRef.current?.setSelectionRange(0,99);
+  window.navigator.clipboard.writeText(password)
+ } , [passwrod])
 
   return (
     <>
-     <h1> hello </h1>
+    <div lassName="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500" >
+     <h1 className='text-white text-center my-3'> Password Generator</h1>
+     <div className='flex shadow rounded-lg  overflow-hidden mb-4'>
+      <input
+       type = "text"
+       value= {password}
+       className='outline-none w-full py-1 px3'
+       placeholder='Password'
+       ref={passwrod}
+       
+      
+      />
+      <button
+      onClick={copyPasswordToClipboard}
+      className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'> copy</button>
+     </div>
+     </div>
     </>
   )
 }
