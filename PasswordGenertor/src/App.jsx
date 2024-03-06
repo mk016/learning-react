a36e55017d4ecd1496817f8e37f43d4b498888be
Mahendra Kumawat <117855423+mk016@users.noreply.github.com>
2024-03-06 13:43:13 +0000

    setpassword

--- PasswordGenertor/src/App.jsx
+++ PasswordGenertor/src/App.jsx
@@ -4,54 +4,55 @@ import { useCallback, useState } from 'react'
 import './App.css'
 
 function App() {
- const [length, setLength ] = useState(8)
- const [numberAllowed , setNumberAllowed] = useState(false);
- const [charAllowed , setCharAllowed] = useState("")
+  const [length, setLength] = useState(8)
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState("")
+  const [password, setPassword] = useState("")
 
- // useRef hook
+  // useRef hook
 
- const passwordRef = useRef(null)
+  const passwordRef = useRef(null)
 
- const passwordGenerator = useCallback(() =>{
-  let pass =""
-  let str =  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-  if (numberAllowed) str += "0123456789"
-  if (charAllowed) str += "!@#$%^&*-_+=[]{}~`"
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if (numberAllowed) str += "0123456789"
+    if (charAllowed) str += "!@#$%^&*-_+=[]{}~`"
 
-  for (let i =1; i<=length; i++){
-    let char = Math.floor(Math.random() * str.length +1)
-    pass += str.charAt(char)
-  }
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass += str.charAt(char)
+    }
 
-   setPassword(pass)
+    setPassword(pass)
 
- }, [length, numberAllowed , charAllowed, setPassword]
- )
+  }, [length, numberAllowed, charAllowed, setPassword]
+  )
 
- const copyPasswordToClipboard = useCallback (() => {
-  passwordRef.current?.setSelectionRange(0,99);
-  window.navigator.clipboard.writeText(password)
- } , [passwrod])
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.setSelectionRange(0, 99);
+    window.navigator.clipboard.writeText(password)
+  }, [passwrod])
 
   return (
     <>
-    <div lassName="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500" >
-     <h1 className='text-white text-center my-3'> Password Generator</h1>
-     <div className='flex shadow rounded-lg  overflow-hidden mb-4'>
-      <input
-       type = "text"
-       value= {password}
-       className='outline-none w-full py-1 px3'
-       placeholder='Password'
-       ref={passwrod}
-       
-      
-      />
-      <button
-      onClick={copyPasswordToClipboard}
-      className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'> copy</button>
-     </div>
-     </div>
+      <div lassName="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500" >
+        <h1 className='text-white text-center my-3'> Password Generator</h1>
+        <div className='flex shadow rounded-lg  overflow-hidden mb-4'>
+          <input
+            type="text"
+            value={password}
+            className='outline-none w-full py-1 px3'
+            placeholder='Password'
+            ref={passwrod}
+
+
+          />
+          <button
+            onClick={copyPasswordToClipboard}
+            className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'> copy</button>
+        </div>
+      </div>
     </>
   )
 }
