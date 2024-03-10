054ccd550dd66b59d121a0ee2783ab669ba80b3c
Mahendra Kumawat <117855423+mk016@users.noreply.github.com>
2024-03-10 21:00:38 +0000

    some file add

---
+++ RouterDom/src/Layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Layout() {
+  return (
+  <>
+    <Header />
+    <Footer />
+    <Home />
+    </>
+  )
+}
+
+export default Layout
--- RouterDom/src/main.jsx
+++ RouterDom/src/main.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from './App.jsx';
+import './index.css';
+import Layout from './Layout.jsx';
+import Home from './Components/Home/Home.jsx';
+import About from './Components/About/About.jsx';
+import Contact from './Components/Contact/Contact.jsx';
+import Header from './Components/Header/Header.jsx';
+import User from './Components/User/User.jsx';
+import Github, { githubInfoLoader } from './Components/Github/Github.jsx';
+
+const router = (
+  <Router> {/* Wrap your routes with Router */}
+    <Layout>
+      <Route path='/' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='user/:userid' element={<User />} />
+      <Route path='github' element={<Github />} />
+    </Layout>
+  </Router>
+);
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {router}
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
